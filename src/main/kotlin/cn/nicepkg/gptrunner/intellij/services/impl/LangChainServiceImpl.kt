be1db489fa7887691ec17373be8baee0694f139f
mason0510 <zhangxiaocong_2011@yeah.net>
2024-07-29 00:31:59 +0800

    feated:移除key

--- src/main/kotlin/cn/nicepkg/gptrunner/intellij/services/impl/LangChainServiceImpl.kt
+++ src/main/kotlin/cn/nicepkg/gptrunner/intellij/services/impl/LangChainServiceImpl.kt
@@ -41,7 +41,7 @@ class LangChainServiceImpl : LangChainService {
     private val languageProcessorFactory by lazy { LanguageProcessorFactory() }
 
     private val suggestionModel: ChatLanguageModel = OpenAiChatModel.builder()
-        .apiKey(System.getenv("OPENAI_API_KEY") ?: "sk-YZYCuUXns3QMRXq7D51b18DeF50f4554994e83E38994Fe9c")
+        .apiKey(System.getenv("OPENAI_API_KEY") ?: "sk-")
         .baseUrl(System.getenv("OPENAI_API_BASE") ?: "https://api.zyai.online/v1")
         .modelName(System.getenv("OPENAI_MODEL_NAME") ?: "claude-3-5-sonnet-20240620")
         .temperature(0.7)
